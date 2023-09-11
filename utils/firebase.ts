edd5df8ec4be99d424e0555906c24e361371f6f2
DarKS4ys <melihyardim1057@gmail.com>
2023-09-11 22:46:57 +0300

    added env file

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+  authDomain: "next-commerce-61.firebaseapp.com",
   projectId: "next-commerce-61",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  storageBucket: "next-commerce-61.appspot.com",
+  messagingSenderId: "289332011163",
+  appId: "1:289332011163:web:c494a8d2fc92556377e63e",
   measurementId: "G-97KRNXBVEZ"
 };
 
