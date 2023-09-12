36b9e419da73ab0abedf96cdcca32bbe28bb4e3b
DarKS4ys <melihyardim1057@gmail.com>
2023-09-12 23:55:22 +0300

    updated products page

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -5,6 +5,10 @@ import { useParams, useRouter } from 'next/navigation';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '@/utils/firebase';
 import Navbar from '@/components/Navbar';
+import Image from 'next/image';
+import Button from '@/components/Button';
+import { BiCart } from 'react-icons/bi';
+import {BsChevronLeft} from 'react-icons/bs'
 
 interface ProductTypes {
     id: string;
@@ -47,10 +51,30 @@ export default function Product() {
     <main className="h-screen w-full flex flex-col">
       <Navbar/>
       {product ? (
-        <div className="flex h-screen justify-center items-center gap-3">
-            <p>{product.id}{product.name}</p>
+        <>
+        <div className="flex flex-col h-screen justify-center items-center gap-2">
+        <button className="flex gap-1 items-center" onClick={() => router.back()}><BsChevronLeft/>Go Back</button>
+          <section className='flex w-[50rem] gap-5 border border-[--border] rounded-lg p-14'>
+            <div className="max-h-[25rem] max-w-[25rem] relative flex">
+              <Image className="rounded-lg" alt="product image" src={product.imageUrl} height={400} width={400}/>
+            </div>
+            <div className="p-8 gap-2 h-full flex flex-col text-xl items-stretch justify-between">
+              <div className="flex flex-col gap-2 h-full text-3xl pb-16 w-72">
+                <h1>{product.name}</h1>
+                <p className="text-base text-muted-foreground">{product.description}</p>
+              </div>
+              <p>{product.price + " $"}</p>
+              <Button className='group hover:scale-110 active:scale-100 hover:bg-white hover:text-black'>
+                Add to cart
+                <div className='group-hover:translate-x-4 transition text-white group-hover:text-black'>
+                <BiCart/>
+                </div>
+              </Button>
+            </div>
+          </section>
           {/* Render other product details */}
         </div>
+      </>
       ) : (
         <div className="flex h-screen justify-center items-center gap-3">
           <p>Loading...</p>
